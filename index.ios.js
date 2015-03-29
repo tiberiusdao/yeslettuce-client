1d3d98a8207061ba4cb1792092a7856670509e28
Andy Jiang <andyjiang@gmail.com>
2015-03-29 10:22:04 -0700

    updated index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,49 +1,69 @@
+
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Yes, Lettuce.
  */
+
 'use strict';
 
+/**
+ * Module dependencies.
+ */
+
 var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+var App = require('./client/app.js');
+var AppRegistry = React;
 
-var client = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js{'\n'}
-          Press Cmd+R to reload
-        </Text>
-      </View>
-    );
-  }
-});
+// var {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View
+// } = React;
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-  },
-});
+/**
+ * Define `App`.
+ */
 
-AppRegistry.registerComponent('client', () => client);
+// var client = React.createClass({
+//   render: function() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js{'\n'}
+//           Press Cmd+R to reload
+//         </Text>
+//       </View>
+//     );
+//   }
+// });
+
+/**
+ * Styles.
+ */
+
+// var styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//   },
+// });
+
+/**
+ * Register `App`.
+ */
+
+AppRegistry.registerComponent('client', () => App);
